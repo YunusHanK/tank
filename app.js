f0aabcb7f585648ea4fc06e37ab47ddc71d43083
YunusHanK <80112647+YunusHanK@users.noreply.github.com>
2024-10-25 12:10:42 +0300

    site

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Add any interactivity or animation controls here.
-    console.log("Website Loaded");
+    const entryScreen = document.getElementById('entry-screen');
+    const mainContent = document.getElementById('main-content');
+
+    // Simulate loading and transition from entry screen to main content
+    setTimeout(() => {
+        entryScreen.style.display = 'none';
+        mainContent.style.display = 'block';
+    }, 3000);  // 3 seconds delay before showing the main content
 });
