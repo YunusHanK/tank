ab71f406ec4c4ff24cc559f2b0bc0a4b574e5a91
YunusHanK <80112647+YunusHanK@users.noreply.github.com>
2024-10-25 14:56:39 +0300

    site

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const loginForm = document.getElementById('login-form');
     const usernameInput = document.getElementById('username');
     const passwordInput = document.getElementById('password');
-    const message = document.getElementById('message');
+    const loginContainer = document.querySelector('.login-container');
     
     loginForm.addEventListener('submit', function(event) {
         event.preventDefault();
@@ -11,22 +11,15 @@ document.addEventListener("DOMContentLoaded", function() {
         const password = passwordInput.value;
         
         if (username === "yunus" && password === "123") {
-            document.body.classList.remove('error');
-            document.body.classList.add('success');
-
-            // Show success message and redirect after 3 seconds
-            message.classList.add('visible');
-            message.textContent = "Giriş yaptınız!";
+            // Trigger circle transformation and rotation
+            loginContainer.classList.add('success');
             
+            // Redirect after 3 seconds
             setTimeout(() => {
-                window.location.href = "https://www.slechi.net.tr"; // Redirect to a new page
+                window.location.href = "https://www.slechi.net.tr";
             }, 3000);
         } else {
-            document.body.classList.remove('success');
-            document.body.classList.add('error');
-            message.classList.add('visible');
-            message.textContent = "Yanlış kullanıcı adı veya şifre!";
-            setTimeout(() => message.classList.remove('visible'), 2000); // Hide after 2 seconds
+            alert("Yanlış kullanıcı adı veya şifre!");
         }
     });
 });
